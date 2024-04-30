9cc83a3c57d4e493cb6b3ba6934c18b015f2e872
suhao <chrissusuhao@gmail.com>
2024-04-30 22:47:27 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,377 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { Button, Card, Input, Radio } from "antd";
+
+function App() {
+  const [chainbowInstalled, setChainBowInstalled] = useState(false);
+  const [connected, setConnected] = useState(false);
+  const [accounts, setAccounts] = useState<string[]>([]);
+  const [publicKey, setPublicKey] = useState("");
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState({
+    confirmed: 0,
+    unconfirmed: 0,
+    total: 0,
+  });
+  const [network, setNetwork] = useState("livenet");
+
+  const getBasicInfo = async () => {
+    const chainbow = (window as any).chainbow;
+    const [address] = await chainbow.getAccounts();
+    setAddress(address);
+
+    const publicKey = await chainbow.getPublicKey();
+    setPublicKey(publicKey);
+
+    const balance = await chainbow.getBalance();
+    setBalance(balance);
+
+    const network = await chainbow.getNetwork();
+    setNetwork(network);
+  };
+
+  const selfRef = useRef<{ accounts: string[] }>({
+    accounts: [],
+  });
+  const self = selfRef.current;
+  const handleAccountsChanged = (_accounts: string[]) => {
+    if (self.accounts[0] === _accounts[0]) {
+      // prevent from triggering twice
+      return;
+    }
+    self.accounts = _accounts;
+    if (_accounts.length > 0) {
+      setAccounts(_accounts);
+      setConnected(true);
+
+      setAddress(_accounts[0]);
+
+      getBasicInfo();
+    } else {
+      setConnected(false);
+    }
+  };
+
+  const handleNetworkChanged = (network: string) => {
+    setNetwork(network);
+    getBasicInfo();
+  };
+
+  useEffect(() => {
+    async function checkChainBow() {
+      let chainbow = (window as any).chainbow;
+
+      for (let i = 1; i < 10 && !chainbow; i += 1) {
+        await new Promise((resolve) => setTimeout(resolve, 100 * i));
+        chainbow = (window as any).chainbow;
+      }
+
+      if (chainbow) {
+        setChainBowInstalled(true);
+      } else if (!chainbow) return;
+
+      chainbow.getAccounts().then((accounts: string[]) => {
+        handleAccountsChanged(accounts);
+      });
+
+      chainbow.on("accountsChanged", handleAccountsChanged);
+      chainbow.on("networkChanged", handleNetworkChanged);
+
+      return () => {
+        chainbow.removeListener("accountsChanged", handleAccountsChanged);
+        chainbow.removeListener("networkChanged", handleNetworkChanged);
+      };
+    }
+
+    checkChainBow().then();
+  }, []);
+
+  if (!chainbowInstalled) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <Button
+              onClick={() => {
+                window.location.href = "https://chainbow.io";
+              }}
+            >
+              Install ChainBow Wallet
+            </Button>
+          </div>
+        </header>
+      </div>
+    );
+  }
+  const chainbow = (window as any).chainbow;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>ChainBow Wallet Demo</p>
+
+        {connected ? (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Card
+              size="small"
+              title="Basic Info"
+              style={{ width: 300, margin: 10 }}
+            >
+              <div style={{ textAlign: "left", marginTop: 10 }}>
+                <div style={{ fontWeight: "bold" }}>Address:</div>
+                <div style={{ wordWrap: "break-word" }}>{address}</div>
+              </div>
+
+              <div style={{ textAlign: "left", marginTop: 10 }}>
+                <div style={{ fontWeight: "bold" }}>PublicKey:</div>
+                <div style={{ wordWrap: "break-word" }}>{publicKey}</div>
+              </div>
+
+              <div style={{ textAlign: "left", marginTop: 10 }}>
+                <div style={{ fontWeight: "bold" }}>Balance: (Satoshis)</div>
+                <div style={{ wordWrap: "break-word" }}>{balance.total}</div>
+              </div>
+            </Card>
+
+            <Card
+              size="small"
+              title="Switch Network"
+              style={{ width: 300, margin: 10 }}
+            >
+              <div style={{ textAlign: "left", marginTop: 10 }}>
+                <div style={{ fontWeight: "bold" }}>Network:</div>
+                <Radio.Group
+                  onChange={async (e) => {
+                    const network = await chainbow.switchNetwork(
+                      e.target.value
+                    );
+                    setNetwork(network);
+                  }}
+                  value={network}
+                >
+                  <Radio value={"livenet"}>livenet</Radio>
+                  <Radio value={"testnet"}>testnet</Radio>
+                </Radio.Group>
+              </div>
+            </Card>
+
+            <SignPsbtCard />
+            <SignMessageCard />
+            <PushTxCard />
+            <PushPsbtCard />
+            <SendBitcoin />
+          </div>
+        ) : (
+          <div>
+            <Button
+              onClick={async () => {
+                const result = await chainbow.requestAccounts();
+                handleAccountsChanged(result);
+              }}
+            >
+              Connect ChainBow Wallet
+            </Button>
+          </div>
+        )}
+      </header>
+    </div>
+  );
+}
+
+function SignPsbtCard() {
+  const [psbtHex, setPsbtHex] = useState("");
+  const [psbtResult, setPsbtResult] = useState("");
+  return (
+    <Card size="small" title="Sign Psbt" style={{ width: 300, margin: 10 }}>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>PsbtHex:</div>
+        <Input
+          defaultValue={psbtHex}
+          onChange={(e) => {
+            setPsbtHex(e.target.value);
+          }}
+        ></Input>
+      </div>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>Result:</div>
+        <div style={{ wordWrap: "break-word" }}>{psbtResult}</div>
+      </div>
+      <Button
+        style={{ marginTop: 10 }}
+        onClick={async () => {
+          try {
+            const psbtResult = await (window as any).chainbow.signPsbt(psbtHex);
+            setPsbtResult(psbtResult);
+          } catch (e) {
+            setPsbtResult((e as any).message);
+          }
+        }}
+      >
+        Sign Psbt
+      </Button>
+    </Card>
+  );
+}
+
+function SignMessageCard() {
+  const [message, setMessage] = useState("hello world~");
+  const [signature, setSignature] = useState("");
+  return (
+    <Card size="small" title="Sign Message" style={{ width: 300, margin: 10 }}>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>Message:</div>
+        <Input
+          defaultValue={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        ></Input>
+      </div>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>Signature:</div>
+        <div style={{ wordWrap: "break-word" }}>{signature}</div>
+      </div>
+      <Button
+        style={{ marginTop: 10 }}
+        onClick={async () => {
+          const signature = await (window as any).chainbow.signMessage(message);
+          setSignature(signature);
+        }}
+      >
+        Sign Message
+      </Button>
+    </Card>
+  );
+}
+
+function PushTxCard() {
+  const [rawtx, setRawtx] = useState("");
+  const [txid, setTxid] = useState("");
+  return (
+    <Card
+      size="small"
+      title="Push Transaction Hex"
+      style={{ width: 300, margin: 10 }}
+    >
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>rawtx:</div>
+        <Input
+          defaultValue={rawtx}
+          onChange={(e) => {
+            setRawtx(e.target.value);
+          }}
+        ></Input>
+      </div>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>txid:</div>
+        <div style={{ wordWrap: "break-word" }}>{txid}</div>
+      </div>
+      <Button
+        style={{ marginTop: 10 }}
+        onClick={async () => {
+          try {
+            const txid = await (window as any).chainbow.pushTx(rawtx);
+            setTxid(txid);
+          } catch (e) {
+            setTxid((e as any).message);
+          }
+        }}
+      >
+        PushTx
+      </Button>
+    </Card>
+  );
+}
+
+function PushPsbtCard() {
+  const [psbtHex, setPsbtHex] = useState("");
+  const [txid, setTxid] = useState("");
+  return (
+    <Card size="small" title="Push Psbt Hex" style={{ width: 300, margin: 10 }}>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>psbt hex:</div>
+        <Input
+          defaultValue={psbtHex}
+          onChange={(e) => {
+            setPsbtHex(e.target.value);
+          }}
+        ></Input>
+      </div>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>txid:</div>
+        <div style={{ wordWrap: "break-word" }}>{txid}</div>
+      </div>
+      <Button
+        style={{ marginTop: 10 }}
+        onClick={async () => {
+          try {
+            const txid = await (window as any).chainbow.pushPsbt(psbtHex);
+            setTxid(txid);
+          } catch (e) {
+            setTxid((e as any).message);
+          }
+        }}
+      >
+        pushPsbt
+      </Button>
+    </Card>
+  );
+}
+
+function SendBitcoin() {
+  const [toAddress, setToAddress] = useState(
+    "tb1qmfla5j7cpdvmswtruldgvjvk87yrflrfsf6hh0"
+  );
+  const [satoshis, setSatoshis] = useState(1000);
+  const [txid, setTxid] = useState("");
+  return (
+    <Card size="small" title="Send Bitcoin" style={{ width: 300, margin: 10 }}>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>Receiver Address:</div>
+        <Input
+          defaultValue={toAddress}
+          onChange={(e) => {
+            setToAddress(e.target.value);
+          }}
+        ></Input>
+      </div>
+
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>Amount: (satoshis)</div>
+        <Input
+          defaultValue={satoshis}
+          onChange={(e) => {
+            setSatoshis(parseInt(e.target.value));
+          }}
+        ></Input>
+      </div>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>txid:</div>
+        <div style={{ wordWrap: "break-word" }}>{txid}</div>
+      </div>
+      <Button
+        style={{ marginTop: 10 }}
+        onClick={async () => {
+          try {
+            const txid = await (window as any).chainbow.sendBitcoin(
+              toAddress,
+              satoshis
+            );
+            setTxid(txid);
+          } catch (e) {
+            setTxid((e as any).message);
+          }
+        }}
+      >
+        SendBitcoin
+      </Button>
+    </Card>
+  );
+}
+
+export default App;
